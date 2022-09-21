b242dc5e1525f173c76d0070352dab63c9aa895a
gsorgi <sorgi.giulia@gmail.com>
2022-09-21 11:21:29 +0200

    subscriber JavaBean

---
+++ src/main/java/purple/dao/Subscriber.java
@@ -0,0 +1,60 @@
+package purple.dao;
+
+import java.util.Objects;
+
+public class Subscriber {
+	private int id;
+	private String name;
+	private String password;
+
+	public Subscriber() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name, password);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Subscriber other = (Subscriber) obj;
+		return id == other.id && Objects.equals(name, other.name) && Objects.equals(password, other.password);
+	}
+
+	@Override
+	public String toString() {
+		return "Subscriber [id=" + id + ", name=" + name + ", password=" + password + "]";
+	}
+
+}
