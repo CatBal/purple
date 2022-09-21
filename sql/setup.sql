c8fd4710f8e1c87e27a3d6ae92a44fa3d0093e5f
SaraClgr <s.colagrossi94@gmail.com>
2022-09-21 14:57:22 +0200

    new table forum

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,4 +1,6 @@
+--subsription
 drop table if exists subscriber;
+drop table if exists forum;
 
 create table subscriber (
 	subscriber_id serial primary key,
@@ -10,4 +12,16 @@ begin;
 	insert into subscriber (name, "password") values ('Sara', 'ciaociao');
 	insert into subscriber (name, "password") values ('Tatiana', 'buonasera');
 	insert into subscriber (name, "password") values ('Beatrice', 'password');
+commit;
+
+create table forum (
+	forum_id serial primary key,
+	name varchar(25), 
+	description varchar(75)
+);
+
+begin;
+	insert into forum (name, description) values
+		('Fumetti', 'Qui parliamo dei fumetti che ci sono piaciuti di più!'),
+	 	('Cucina', 'Spazio per confrontarci sulle nostre ricette!');
 commit;
