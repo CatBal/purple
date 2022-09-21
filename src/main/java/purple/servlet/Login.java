395a6f3052da4a0222992cda358a2ddd6f242dc6
gsorgi <sorgi.giulia@gmail.com>
2022-09-21 10:50:23 +0200

    added parameters and log

--- src/main/java/purple/servlet/Login.java
+++ src/main/java/purple/servlet/Login.java
@@ -7,19 +7,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
- * Servlet implementation class Login
+ * Login management, we expect user to pass us parameters using username and password
  */
 @WebServlet("/login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
+	private static final Logger log = LogManager.getLogger(Login.class);
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		log.info(username + ", " + password);
 		request.getRequestDispatcher("login.jsp").forward(request, response);
 	}
 
