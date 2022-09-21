e97e355e8e81cdae22b3c615b27665909bbed67b
SaraClgr <s.colagrossi94@gmail.com>
2022-09-21 17:05:16 +0200

    table message table subscription

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,4 +1,5 @@
---subsription
+drop table if exists message;
+drop table if exists "subscription";
 drop table if exists subscriber;
 drop table if exists forum;
 
@@ -25,3 +26,32 @@ begin;
 		('Fumetti', 'Qui parliamo dei fumetti che ci sono piaciuti di più!'),
 	 	('Cucina', 'Spazio per confrontarci sulle nostre ricette!');
 commit;
+
+create table message (
+	message_id serial primary key,
+	text varchar(200), 
+	subscriber_id int not null,
+	forum_id int not null,
+	constraint message_subscriber_fk foreign key (subscriber_id) references subscriber (subscriber_id),
+	constraint message_forum_fk foreign key (forum_id) references forum (forum_id)
+);
+
+insert into message (text, subscriber_id, forum_id) values 
+	('ciao, buona giornata!', 1, 2),
+	('arrivederci!', 1, 1);
+
+create table "subscription" (
+	subscriber_id int not null,
+	forum_id int not null,
+	constraint subscription_subscriber_fk foreign key (subscriber_id) references subscriber (subscriber_id),
+	constraint subscription_forum_fk foreign key (forum_id) references forum (forum_id)
+);
+
+insert into "subscription" (subscriber_id, forum_id) values
+	(1, 2),
+	(1, 1),
+	(2, 2);
+
+
+
+
